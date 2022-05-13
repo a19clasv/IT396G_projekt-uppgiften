d1ba1d415253b00b38c3ed27a8046e0f844eee99
Claes Svensson <a19clasv@student.his.se>
2022-05-13 23:39:31 +0200

    Added Dish.java with variables ID, login, name, cost, course and tip. All the variables have getters and tip has a setter

---
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/Dish.java
@@ -0,0 +1,47 @@
+package com.example.it396gprojekt_uppgiften;
+
+public class Dish {
+    private Long ID;
+    private String login;
+    private String name;
+    private String cost;
+    private String course;
+    private int tip;
+
+    public Dish(Long ID, String login, String name, String cost, String course, int tip) {
+        this.ID = ID;
+        this.login = login;
+        this.name = name;
+        this.cost = cost;
+        this.course = course;
+        this.tip = tip;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getTip() {
+        return tip;
+    }
+
+    public void setTip(int tip) {
+        this.tip = tip;
+    }
+}
