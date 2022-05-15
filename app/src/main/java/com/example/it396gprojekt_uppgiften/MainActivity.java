320befdf4824630983d2db14edcc9f02e4335195
Claes Svensson <a19clasv@student.his.se>
2022-05-16 00:19:54 +0200

    Added Button in MainActivity that starts AboutActivity when clicked

--- app/src/main/java/com/example/it396gprojekt_uppgiften/MainActivity.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.it396gprojekt_uppgiften;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
