f4bee587b153546c28715a4368e951c76911b0d8
Claes Svensson <a19clasv@student.his.se>
2022-05-16 00:09:42 +0200

    Added a webview that loads about.html in AboutActivity.java

--- app/src/main/java/com/example/it396gprojekt_uppgiften/AboutActivity.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/AboutActivity.java
@@ -3,12 +3,22 @@ package com.example.it396gprojekt_uppgiften;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        myWebView = findViewById(R.id.my_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("file:///android_asset/about.html");
     }
 }
