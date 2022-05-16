61aa55bc4b2bb69294132396d3a0f2c2fd16eaf3
Claes Svensson <a19clasv@student.his.se>
2022-05-17 00:58:56 +0200

    Added restaurant and location variables to Dish. Also added getters for the variables and changed info function to include them

--- app/src/main/java/com/example/it396gprojekt_uppgiften/Dish.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/Dish.java
@@ -6,14 +6,18 @@ public class Dish {
     private String name;
     private String cost;
     private String course;
+    private String restaurant;
+    private String location;
     private int tip;
 
-    public Dish(Long ID, String login, String name, String cost, String course, int tip) {
+    public Dish(Long ID, String login, String name, String cost, String course, String restaurant, String location, int tip) {
         this.ID = ID;
         this.login = login;
         this.name = name;
         this.cost = cost;
         this.course = course;
+        this.restaurant = restaurant;
+        this.location = location;
         this.tip = tip;
     }
 
@@ -37,6 +41,14 @@ public class Dish {
         return course;
     }
 
+    public String getRestaurant() {
+        return restaurant;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
     public int getTip() {
         return tip;
     }
@@ -47,7 +59,8 @@ public class Dish {
 
     public String info(){
         String tmp=new String();
-        tmp ="ID: " + getID() + ". Login: " + getLogin() + ". Name: " + getName() + ". Cost: " + getCost() + ". Course: " + getCourse() + "Tip" + getTip();
+        tmp ="ID: " + getID() + ". Login: " + getLogin() + ". Name: " + getName() + ". Cost: " + getCost() +
+                ". Course: " + getCourse() + "Restaurant" + getRestaurant() + "Location" + getLocation() + "Tip" + getTip();
         return  tmp;
     }
 }
