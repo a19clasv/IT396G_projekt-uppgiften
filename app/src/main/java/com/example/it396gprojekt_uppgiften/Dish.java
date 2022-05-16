05bef8f321463d92675ad1d32d718e772498b19c
Claes Svensson <a19clasv@student.his.se>
2022-05-17 01:18:29 +0200

    Removed course, location and tip variables along with any usage of them. Also changed "Restaurant" to ". Restaurant" in the info function

--- app/src/main/java/com/example/it396gprojekt_uppgiften/Dish.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/Dish.java
@@ -5,20 +5,14 @@ public class Dish {
     private String login;
     private String name;
     private int cost;
-    private String course;
     private String restaurant;
-    private String location;
-    private int tip;
 
-    public Dish(Long ID, String login, String name, int cost, String course, String restaurant, String location, int tip) {
+    public Dish(Long ID, String login, String name, int cost, String restaurant) {
         this.ID = ID;
         this.login = login;
         this.name = name;
         this.cost = cost;
-        this.course = course;
         this.restaurant = restaurant;
-        this.location = location;
-        this.tip = tip;
     }
 
     public Long getID() {
@@ -37,30 +31,13 @@ public class Dish {
         return cost;
     }
 
-    public String getCourse() {
-        return course;
-    }
-
     public String getRestaurant() {
         return restaurant;
     }
 
-    public String getLocation() {
-        return location;
-    }
-
-    public int getTip() {
-        return tip;
-    }
-
-    public void setTip(int tip) {
-        this.tip = tip;
-    }
-
     public String info(){
         String tmp=new String();
-        tmp ="ID: " + getID() + ". Login: " + getLogin() + ". Name: " + getName() + ". Cost: " + getCost() +
-                ". Course: " + getCourse() + "Restaurant" + getRestaurant() + "Location" + getLocation() + "Tip" + getTip();
+        tmp ="ID: " + getID() + ". Login: " + getLogin() + ". Name: " + getName() + ". Cost: " + getCost() + ". Restaurant" + getRestaurant();
         return  tmp;
     }
 }
