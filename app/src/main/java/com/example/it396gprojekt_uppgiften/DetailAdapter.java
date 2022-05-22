d0b11da2209115d8a4862152214ff3bbf5f2b39e
Claes Svensson <a19clasv@student.his.se>
2022-05-23 00:40:05 +0200

    Removed click listeners that start DetailActivity and context that was required for it from DetailAdapter

--- app/src/main/java/com/example/it396gprojekt_uppgiften/DetailAdapter.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/DetailAdapter.java
@@ -1,7 +1,5 @@
 package com.example.it396gprojekt_uppgiften;
 
-import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +12,9 @@ import java.util.ArrayList;
 public class DetailAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     private ArrayList<MenuItem> menuItems;
-    private Context context;
 
-    public  DetailAdapter(ArrayList<MenuItem> menuItems, Context context){
+    public  DetailAdapter(ArrayList<MenuItem> menuItems){
         this.menuItems = menuItems;
-        this.context = context;
     }
 
     @NonNull
@@ -31,29 +27,8 @@ public class DetailAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.nameTextView.setText("Name: " + menuItems.get(position).getName());
-        holder.nameTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, DetailActivity.class);
-                context.startActivity(intent);
-            }
-        });
         holder.costTextView.setText("Cost: " + menuItems.get(position).getCost() + " kr");
-        holder.costTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, DetailActivity.class);
-                context.startActivity(intent);
-            }
-        });
         holder.restaurantTextView.setText("Restaurant: " + menuItems.get(position).getCompany());
-        holder.restaurantTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, DetailActivity.class);
-                context.startActivity(intent);
-            }
-        });
     }
 
     @Override
