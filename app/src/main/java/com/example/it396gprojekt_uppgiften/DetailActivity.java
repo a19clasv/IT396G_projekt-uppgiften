d5005f81a0c0718f87aee70fa3fd9c2bfc865132
Claes Svensson <a19clasv@student.his.se>
2022-05-23 00:29:20 +0200

    Copied about activity's back button over to detail activity

--- app/src/main/java/com/example/it396gprojekt_uppgiften/DetailActivity.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/DetailActivity.java
@@ -3,12 +3,24 @@ package com.example.it396gprojekt_uppgiften;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
