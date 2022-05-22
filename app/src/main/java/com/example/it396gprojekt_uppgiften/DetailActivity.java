54c7997f3d5c27eabff2015f6b8faa8b6ee5698f
Claes Svensson <a19clasv@student.his.se>
2022-05-23 00:00:01 +0200

    Seed code for DetailActivity

---
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.it396gprojekt_uppgiften;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
