f6bb2693a251b81c18fed339ff3ae9e6b307d9e8
Claes Svensson <a19clasv@student.his.se>
2022-05-22 23:50:22 +0200

    Added " kr" to the end of the text for the cost that MyAdapter sets

--- app/src/main/java/com/example/it396gprojekt_uppgiften/MyAdapter.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/MyAdapter.java
@@ -27,7 +27,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.nameTextView.setText("Name: " + menuItems.get(position).getName());
-        holder.costTextView.setText("Cost: " + menuItems.get(position).getCost());
+        holder.costTextView.setText("Cost: " + menuItems.get(position).getCost() + " kr");
         holder.restaurantTextView.setText("Restaurant: " + menuItems.get(position).getCompany());
     }
 
