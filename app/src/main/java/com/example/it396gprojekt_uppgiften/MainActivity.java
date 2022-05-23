d59f7894ca36c9ca95c7c001d6dd59f69f03b693
Claes Svensson <a19clasv@student.his.se>
2022-05-23 14:15:19 +0200

    MainActivity fetches the saved search filters on resuming

--- app/src/main/java/com/example/it396gprojekt_uppgiften/MainActivity.java
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,11 +23,14 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private ArrayList<MenuItem> menuItems;
     private RecyclerView.Adapter adapter;
     private RecyclerView recyclerView;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
 
         aboutButton = findViewById(R.id.about_button);
         aboutButton.setOnClickListener(new View.OnClickListener() {
@@ -60,5 +64,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         menuItems = gson.fromJson(json, type);
         adapter = new MyAdapter(menuItems, MainActivity.this);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = preferences.getString("name", "");
+        String restaurant = preferences.getString("restaurant", "");
+        String cost = preferences.getString("cost", "");
+        String costSign = preferences.getString("costSign", "");
     }
 }
