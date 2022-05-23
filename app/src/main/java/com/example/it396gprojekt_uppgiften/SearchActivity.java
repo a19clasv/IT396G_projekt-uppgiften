6f63acf9e355c84f3dc17f30a5c18d8e87034bdf
Claes Svensson <a19clasv@student.his.se>
2022-05-23 07:39:28 +0200

    Seed code for search activity

---
+++ app/src/main/java/com/example/it396gprojekt_uppgiften/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.it396gprojekt_uppgiften;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
